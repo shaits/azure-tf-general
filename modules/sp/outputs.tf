0df943cb194cfa9dfb5c435bb43348622c73827e
tz24r5_gme <shay.tsabar@gm.com>
2025-09-27 01:11:58 +0300

    added uami_client_id as sp output:

--- modules/sp/outputs.tf
+++ modules/sp/outputs.tf
@@ -2,3 +2,8 @@ output "eso_id" {
   description = "ID of the workload identity"
   value       = azurerm_user_assigned_identity.eso.id
 }
+
+output "uami_client_id" {
+  value = azurerm_user_assigned_identity.eso.client_id
+}
+
